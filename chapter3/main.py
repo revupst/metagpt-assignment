8296e1788c37313a32f0bf74100e4531bcd06539
revupst <revup123456@gmail.com>
2024-01-16 14:29:42 +0800

    Chapter3: Use LLM to generate action list

--- chapter3/main.py
+++ chapter3/main.py
@@ -5,6 +5,7 @@ from metagpt.actions import Action
 from metagpt.roles import Role
 from metagpt.schema import Message
 from metagpt.logs import logger
+from metagpt.utils.common import OutputParser
 
 
 class SimplePrint(Action):
@@ -13,10 +14,30 @@ class SimplePrint(Action):
     def __init__(self, name="SimplePrint", context=None, llm=None):
         super().__init__(name=name, context=context, llm=llm)
 
-    async def run(self, content: str) -> str:
+    async def run(self, content: str, need_generate: bool) -> list:
         logger.info(f"SimplePrint({SimplePrint._count}): {content}")
         SimplePrint._count += 1
-        return content
+        if need_generate:
+            return await self._generate_actions()
+        return []
+
+    async def _generate_actions(self) -> list:
+        PROMPT = """
+        Please generate a new action list for MetaGPT, strictly following the following requirements:
+        - The result must be a json object, and the result list is the value ,the key is 'actions'.
+        - Each action is named SimplePrint.
+        - The length of the list is 3.
+        - Don't need to write python code, just return the json object.
+        Example:
+        {'actions': ['SimplePrint', 'SimplePrint']}
+        """
+        resp = await self._aask(prompt=PROMPT)
+        ret = OutputParser.extract_struct(resp, "dict")
+        actions_strs = ret["actions"]
+        logger.info(actions_strs)
+        actions = [globals()[s] for s in actions_strs]
+        logger.info(actions)
+        return actions
 
 
 class SimplePrinter(Role):
@@ -24,10 +45,8 @@ class SimplePrinter(Role):
         self,
         name: str = "SimplePrinter",
         profile: str = "Simple Printer",
-        goal: str = "Generate and print new random messages",
-        constraints: str = "Result must be Chinese",
     ):
-        super().__init__(name=name, profile=profile, goal=goal, constraints=constraints)
+        super().__init__(name=name, profile=profile)
         self._init_actions([SimplePrint, SimplePrint, SimplePrint])
         self._rc.todo = None
         self._has_generated = False
@@ -45,27 +64,21 @@ class SimplePrinter(Role):
         if self._rc.state + 1 < len(self._states):
             self._set_state(self._rc.state + 1)
         else:
-            if self._has_generated == False:
-                logger.info("generate new")
-                await self._generate()
-                self._has_generated = True
-                self._rc.todo = None
-                self._set_state(0)
-            else:
-                self._rc.todo = None
+            self._rc.todo = None
 
-    async def _generate(self) -> None:
-        if self._has_generated == False:
-            self._has_generated = True
-            self._init_actions([SimplePrint, SimplePrint, SimplePrint])
-
-    async def _act(self) -> Message:
+    async def _act(self):
         logger.info(f"{self._setting}: 准备 {self._rc.todo}")
         todo = self._rc.todo
-        result = await todo.run(f"state {self._rc.state}")
-        msg = Message(content=result, role=self.profile)
-        self._rc.memory.add(msg)
-        return msg
+
+        logger.info(f"state len: {self._rc.state}:{len(self._states)}")
+
+        if self._rc.state + 1 == len(self._states) and self._has_generated == False:
+            self._has_generated = True
+            actions = await todo.run(f"state {self._rc.state}", need_generate=True)
+            self._init_actions(actions)
+            self._rc.todo = None
+        else:
+            result = await todo.run(f"state {self._rc.state}", need_generate=False)
 
     async def _react(self) -> Message:
         while True:
